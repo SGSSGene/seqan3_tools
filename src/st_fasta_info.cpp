5902cbe6328aabb6875221a5f48dda0595d23918
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-08-12 12:11:33 +0200

    feat: new mode for st_fasta_info

--- src/st_fasta_info.cpp
+++ src/st_fasta_info.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <seqan3/alphabet/views/all.hpp>
 #include <seqan3/alphabet/nucleotide/dna5.hpp>
 #include <seqan3/argument_parser/all.hpp>
 #include <seqan3/core/debug_stream.hpp>
@@ -16,7 +17,15 @@ int main(int argc, char const* const* argv) {
                                  seqan3::input_file_validator{{"fa", "fasta", "fna"}});
 
     std::string mode{"unknown"};
-    parser.add_positional_option(mode, "Please provide a mode: names, lengths");
+    parser.add_positional_option(mode, "Please provide a mode: names, lengths, view");
+
+    size_t queryId{}, pos{}, len{100};
+    bool revCompl{false};
+
+    parser.add_option(queryId, '\0', "id",        "(view) number of query");
+    parser.add_option(pos,     '\0', "start",     "(view) position where to start printing");
+    parser.add_option(len,     '\0', "len",       "(view) how many chars to print");
+    parser.add_flag(revCompl,  '\0', "rev-compl", "(view) consider artifical revComple (uneven numbers are artifical)");
 
 
     try {
@@ -36,6 +45,25 @@ int main(int argc, char const* const* argv) {
         for (auto & record : fin) {
             std::cout << record.id() << "\n";
         }
+    } else if (mode == "view") {
+        size_t i{};
+        for (auto & record : fin) {
+            if (!revCompl) if (i++ != queryId) continue;
+            if (revCompl) if (i != queryId and i+1 != queryId) { i+=2; continue; }
+
+            auto seq = record.sequence();
+            if (revCompl and (queryId%2 != 0)) {
+                seq = seq | std::views::reverse | seqan3::views::complement | seqan3::ranges::to<std::vector>();
+            }
+
+            for (size_t j{pos}; j < pos+len; ++j) {
+                if (j >= seq.size()) std::cout << "_";
+                else std::cout << seq[j].to_char();
+            }
+            std::cout << "\n";
+            break;
+        }
+
     } else {
         std::cout << "unknown mode: " << mode << "\n";
     }
