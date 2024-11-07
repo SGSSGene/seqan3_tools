8c83254066a978914f8ab3d6007fd8861cf61ae2
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-11-07 10:09:03 +0100

    feat: option to chose the seed

--- src/st_dna5todna4.cpp
+++ src/st_dna5todna4.cpp
@@ -20,6 +20,9 @@ int main(int argc, char const* const* argv) {
     parser.add_positional_option(infile, "Please provide a fasta file.",
                                  seqan3::input_file_validator{{"fa", "fasta", "fna"}});
 
+    int seed{-1};
+    parser.add_option(seed, '\0', "seed", "seed to generate data, if value -1 current time is being used.");
+
     try {
          parser.parse();
     } catch (seqan3::argument_parser_error const& ext) {
@@ -31,7 +34,10 @@ int main(int argc, char const* const* argv) {
     seqan3::sequence_file_input fin{infile};
     seqan3::sequence_file_output fout{std::cout, seqan3::format_fasta{}};
 
-    std::srand(std::time(0));
+    if (seed == -1) {
+        seed = time(0);
+    }
+    std::srand(seed);
     // iterate through all sequences in input file
     for (auto & record : fin) {
         std::vector<seqan3::dna5> sequence;
